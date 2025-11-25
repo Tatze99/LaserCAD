027d1553999982e0fd3fdc15c8cf7b98d0fcd80e
Martin B. <50291236+Tatze99@users.noreply.github.com>
2025-11-25 01:34:47 +0100

    Implementation of new Design

--- A3_sketch_generalized_V2.py
+++ A3_sketch_generalized_V2.py
@@ -2,13 +2,10 @@
 """
 Created on Wed Feb 14 12:39:52 2024
 
-@author: 12816
+@author: Martin
 """
 
 ##################################
-UseNormalDesign = False
-UseCompactDesign = not UseNormalDesign
-UseTwoTelescopes = True
 UsePolRotator = False
 ##################################
 
@@ -40,13 +37,13 @@ def get_TFP_distances(TFP_ydist, TFP_angle):
     TFP_delta = TFP_dist - TFP_ydist - TFP_xdist
     
     TFP_Lam = 0.1*TFP_dist
-    Lam_PC = 0.8*TFP_dist  # 0.6 before
-    PC_TFP = TFP_dist - TFP_Lam - Lam_PC
+    Lam_PC = 0.7*TFP_dist  # 0.6 before
+    dist_PC_TFP = TFP_dist - TFP_Lam - Lam_PC
     
-    return TFP_dist, TFP_xdist, TFP_delta, TFP_Lam, Lam_PC, PC_TFP
+    return TFP_dist, TFP_xdist, TFP_delta, TFP_Lam, Lam_PC, dist_PC_TFP
 
 rotate_axis = np.pi
-offset_axis = (950+160,500-60,20)
+offset_axis = (950+200,300,20)
 
 class Newport_Mirror(Mirror):
     def __init__(self, name="Newport Mirror", aperture=25.4, **kwargs):
@@ -99,10 +96,6 @@ class Cylindric_Crystal(Component):
     self.freecad_model = model_mirror
     self.pos += offset_axis
 
-# pockels_cell = Component(name="Pockels Cell")
-# pockels_cell.draw_dict["stl_file"]= rf"{thisfolder}\mount_meshes\A2_mounts\Pockels_cell.stl"
-# pockels_cell.freecad_model = load_STL
-
 vacuum_tube = Component(name="Vacuum Tube")
 vacuum_tube.draw_dict["stl_file"]= rf"{thisfolder}\mount_meshes\special_mount\Vacuum_Tube_1300mm.stl"
 vacuum_tube.freecad_model = load_STL
@@ -122,100 +115,64 @@ beam = Beam(radius=5, angle=0)
 beam.pos=[0,0,0]
 
 if UsePolRotator:
-    g = 360 # object distance: distance between the pump region and the first curved mirror
+    g = 530 # object distance: distance between the pump region and the first curved mirror
     length_diff = Pol_Rotater.length_diff
     print(f"Using Polarization Rotator with length difference of {length_diff}mm")
     if UseNormalDesign: g -= 20
 else: 
-    g = 510#520
+    g = 550#520
     length_diff = 0
     if UseNormalDesign: g = 490
     
-    
+ydist_M2_M3 = 70
 
 # Calculation of the image distance
 b = ((f1-g)*f2**2 + f1**2 * f2) / (f1**2)
 
 pump_angle = 90   # input-output angle on the pump mirrors, standard: 90
-tele_angle1 = 6   # opening angle for coupling into telescope
-tele_angle2 = 6   # opening angle for couling into telescope # 4 before
+tele_angle1 = 4   # opening angle for coupling into telescope
+tele_angle2 = 4   # opening angle for couling into telescope # 4 before
 
-ydist_M2_M3 = 60 if UseCompactDesign else 0 # y-distance between M2 and M3, standard: 60mm
+xdist_R1_M1 = 190 # cut mirror x-distance from spherical mirrors
+ydist_R2_M2 = 330 # cut mirror y-distance from spherical mirrors
 
-xdist_R1_M1 = 190-ydist_M2_M3/2 # cut mirror x-distance from spherical mirrors
-ydist_R2_M2 = 190+ydist_M2_M3/2 # cut mirror y-distance from spherical mirrors
-
-dist_P2_M4 = 70  # distance from pump mirror P2 to M4
+dist_P2_M4 = 60  # distance from pump mirror P2 to M4
 dist_R1_M1 = xdist_R1_M1 / np.cos(rad(tele_angle1)) # propagation distance to first spherical mirror
 dist_R2_M2 = ydist_R2_M2 / np.cos(rad(tele_angle2)) # propagation distance to second spherical mirror
 
 ydist_R1_M1 = np.sqrt(dist_R1_M1**2 - xdist_R1_M1**2)
 xdist_R2_M2 = np.sqrt(dist_R2_M2**2 - ydist_R2_M2**2)
 
-
 TFP_angle = 66
 TFP_angle = rad(2*TFP_angle - 90)
-
-difference_to_ideal_imaging = 150 - length_diff # propagation difference to ideal imaging (after roundtrip)
             
-cavity_length = f1 + f2 + g + b + difference_to_ideal_imaging   # total cavity length, g-object distance, b-image distance
+cavity_length = f1 + f2 + g + b  # total cavity length, g-object distance, b-image distance
             
 pump_dist = 100    # 150            # length of pump section
-P2_shift = 0
-dist_P2_M1 = g-dist_R1_M1-dist_P2_M4-pump_dist/2       # distance from P2 to M1
+dist_M4_M1 = g-dist_P2_M4-dist_R1_M1-pump_dist/2       # distance from P2 to M1
 xdist_M3_TFP1 = 160                            # x-distance from M3 to TFP1
-dist_vacuum_tube = xdist_R1_M1 + 70
+dist_vacuum_tube = xdist_R1_M1 + 150
 
+# ydist: difference between beam y-position at TFP2 and the vacuum tube
 
-if UseNormalDesign:
-    TFP_ydist = 170
-    TFP_dist, TFP_xdist, TFP_delta, TFP_Lam, Lam_PC, PC_TFP = get_TFP_distances(TFP_ydist, TFP_angle)
-    
-    xdist_P1_TFP1 = f1 + f2 - xdist_R1_M1 - ydist_R2_M2 - xdist_M3_TFP1 - pump_dist
-    ydist = 0.5*(cavity_length - f1 - f2 - TFP_delta - (xdist_P1_TFP1 + pump_dist + xdist_M3_TFP1) - (dist_R1_M1 + dist_R2_M2 + ydist_R1_M1 + xdist_R2_M2))
-    M2 = Newport_Mirror(phi=-90-tele_angle2, name="M2") # cut mirror 2
-    M3 = Newport_Mirror(phi=90, name="M3") # mirror to TFP1
+ydist = pump_dist + dist_M4_M1 - ydist_R1_M1 
+TFP_ydist = ydist - ydist_M2_M3
+TFP_dist, TFP_xdist, TFP_delta, TFP_Lam, Lam_PC, dist_PC_TFP = get_TFP_distances(TFP_ydist, TFP_angle)
 
-else: # UseCompactDesign
-    # ydist: difference between beam y-position at TFP2 and the vacuum tube
-    # ydist_M2_M3: y-distance between M2 and M3
-    # ydist_P2_TFP1: y-distance between pump mirror P2 and TFP1
-    # xdist_P1_TFP1: x-distance between pump mirror P2 and TFP1
-
-    pump_angle_rad = rad(180-pump_angle)
-    
-    # is 1 for a pump angle of 90°
-    pump_angle_correction_factor = 1/np.tan(pump_angle_rad) + 1/np.sin(pump_angle_rad)
-    x_correction_by_angled_pump = dist_P2_M1 * np.cos(rad(180-pump_angle))  
-    
-    ydist_P2_TFP1 = (dist_P2_M1 * np.sin(rad(180-pump_angle)) - ydist_R1_M1 - ydist_M2_M3) * pump_angle_correction_factor
-    xdist_P1_TFP1 = f1 + f2 + xdist_R2_M2 - dist_R2_M2 - xdist_R1_M1 + x_correction_by_angled_pump - pump_dist - xdist_M3_TFP1
-    
-    distance_to_TFP1 = g + f1 + f2 + ydist_R2_M2 - ydist_M2_M3 + xdist_M3_TFP1
-
-    TFP_ydist = (cavity_length - xdist_P1_TFP1 - distance_to_TFP1 - pump_dist/2 + ydist_P2_TFP1) / (1/np.cos(TFP_angle)- np.tan(TFP_angle) + pump_angle_correction_factor)
-    TFP_dist, TFP_xdist, TFP_delta, TFP_Lam, Lam_PC, PC_TFP = get_TFP_distances(TFP_ydist, TFP_angle)
-    ydist = TFP_ydist + ydist_M2_M3
-    
-    M2 = Newport_Mirror(phi=90-tele_angle2, name="M2, cut mirror 2") # cut mirror 2
-    M3 = Newport_Mirror(phi=-90, name="M3, mirror towards tfp 1") # mirror to TFP1
-
-
-dist_M4_P1 = (ydist - dist_P2_M1*np.sin(rad(180-pump_angle)) + ydist_R1_M1) / np.sin(rad(pump_angle))  # M4 to P1 (pump mirror 1)
-dist_TFP2_P1 = xdist_P1_TFP1 - TFP_xdist + dist_M4_P1 * np.cos(rad(180-pump_angle)) - dist_P2_M4 # TFP2 to M4
 
 print(f"lambda PC distance = {Lam_PC}mm")
 print("Cavity")
-print(f"g = {g}mm, b = {b}mm, difference to ideal image={difference_to_ideal_imaging}mm")
+print(f"g = {g}mm, b = {b}mm")
 
 P1 = Newport_Mirror(name="pump mirror 1", phi=pump_angle, aperture=25.4*2) # pump mirror 1
-P1.set_mount(Adapter_2inch(angle=90))
+# P1.set_mount(Adapter_2inch(angle=90))
 P2 = Newport_Mirror(name="pump mirror 2", phi=-pump_angle, aperture=25.4*2)  # pump mirror 2
 M1 = Newport_Mirror(name="M1, cut mirror 1", phi=-pump_angle-tele_angle1) # cut mirror 1
-
+M2 = Newport_Mirror(phi=90, name="M2, cut mirror 2") # cut mirror 2
+M3 = Newport_Mirror(phi=-90+tele_angle2, name="M3, mirror towards tfp 1") # mirror to TFP1
 M4 = Newport_Mirror(name="M4, after pump mirror 2", phi=pump_angle) # mirror after TFP2
 R1 = Newport_Curved_Mirror(name=f"R1, f={f1:.0f}mm", phi=-180+tele_angle1, radius=r1)
-R2 = Newport_Curved_Mirror(name=f"R2, f={f2:.0f}mm", phi=-180+tele_angle2, radius=r2, aperture=25.4*3)
+R2 = Newport_Curved_Mirror(name=f"R2, f={f2:.0f}mm", phi=-180-tele_angle2, radius=r2, aperture=25.4*3)
 TFP1 = Newport_Mirror(name="TFP1 (Input)", phi=-90+deg(TFP_angle), aperture=25.4*2)
 TFP2 = Newport_Mirror(name="TFP2 (Output)", phi=90-deg(TFP_angle), aperture=25.4*2)
 pockels_cell = Pockels_Cell_Thick(name="Pockels Cell", mount_name="Pockels_cell_thick")
@@ -223,38 +180,26 @@ pockels_cell = Pockels_Cell_Thick(name="Pockels Cell", mount_name="Pockels_cell_
 Setup = Composition(name="A3")
 Setup.set_light_source(beam)
 Setup.pos += offset_axis
+
+pos_start = Setup.pos.copy()
 Setup.normal = (0,1,0)
 Setup.propagate(pump_dist/2)
 Setup.add_on_axis(P2)
 Setup.propagate(dist_P2_M4)
 Setup.add_on_axis(M4)
-Setup.propagate(dist_P2_M1)
+Setup.propagate(dist_M4_M1)
+Setup.add_on_axis(M1)
 Setup.propagate(dist_R1_M1)
 Setup.add_on_axis(R1)
-Setup.propagate(dist_R1_M1)
-Setup.add_on_axis(M1)
+print(f"distance to first curved mirror: {Setup.optical_path_length():.2f}mm, g={g}mm")
 Setup.propagate(dist_vacuum_tube)
 Setup.add_on_axis(vacuum_tube)
-
-if UseNormalDesign:    
-    Setup.propagate(f1+f2-dist_vacuum_tube-dist_R1_M1)
-    Setup.add_on_axis(R2)
-    Setup.propagate(dist_R2_M2)
-    Setup.add_on_axis(M2)
-    Setup.propagate(xdist_R2_M2+ydist-TFP_ydist)
-    print(f"Image Distance = {dist_R2_M2+xdist_R2_M2+ydist-TFP_ydist + xdist_M3_TFP1 + TFP_dist + dist_TFP2_P1+ dist_M4_P1 + pump_dist/2 - difference_to_ideal_imaging} (adding all lengths) = {b} (b)")
-    
-
-elif UseCompactDesign: 
-    Setup.propagate(f1+f2-dist_vacuum_tube-dist_R2_M2-dist_R1_M1)
-    Setup.add_on_axis(M2)
-    Setup.propagate(dist_R2_M2)
-    Setup.add_on_axis(R2)
-    Setup.propagate(ydist_R2_M2-ydist_M2_M3)
-    print(f"Image Distance = {ydist_R2_M2-ydist_M2_M3+xdist_M3_TFP1+TFP_dist+dist_TFP2_P1+dist_M4_P1+pump_dist/2 - difference_to_ideal_imaging} (adding all lengths) = {b} (b)")
-    # Setup.propagate(ydist_R2_M2-ydist+TFP_ydist)
-
-    
+Setup.propagate(f1+f2-dist_vacuum_tube-dist_R2_M2)
+Setup.add_on_axis(M2)
+Setup.propagate(dist_R2_M2)
+Setup.add_on_axis(R2)
+print(f"distance to first second curved mirror: {Setup.optical_path_length():.2f}mm, g+f1+f2={g+f1+f2}mm")
+Setup.propagate(ydist_R2_M2-ydist_M2_M3)
 Setup.add_on_axis(M3)
 Setup.propagate(xdist_M3_TFP1)
 Setup.add_on_axis(TFP1)
@@ -271,22 +216,28 @@ Setup.add_on_axis(pockels_cell)
 pockels_cell.rotate((0,0,1), np.pi)
 pockels_cell.rotate(pockels_cell.normal, np.pi)
 
-Setup.propagate(PC_TFP)
+Setup.propagate(dist_PC_TFP)
 Setup.add_on_axis(TFP2)
-Setup.propagate(dist_TFP2_P1+P2_shift)
+dist_TFP2_P1 = np.linalg.norm(TFP2.pos - pos_start)
+
+Setup.propagate(dist_TFP2_P1)
 Setup.add_on_axis(P1)
-Setup.propagate(pump_dist/2-P2_shift)
+Setup.propagate(pump_dist/2)
 
-print(f"optical path length = {Setup.optical_path_length():.2f}mm, cavity length = {cavity_length:.2f}mm\n")
+image_distance = ydist_R2_M2-ydist_M2_M3+xdist_M3_TFP1+TFP_dist+dist_TFP2_P1+pump_dist/2
 
-M3.Mount.mount_list[0].flip(90)
+print(f"Image Distance = {image_distance:.2f} (adding all lengths) = {b:.2f} (b)")
+print(f"distance to ideal imaging = {Setup.optical_path_length()+length_diff - cavity_length:.2f}mm")
+print(f"optical path length = {Setup.optical_path_length()+length_diff:.2f}mm, cavity length = {cavity_length:.2f}mm\n")
+
+# M3.Mount.mount_list[0].flip(90)
 # P1.Mount.mount_list[0].flip(90)
 
 
 """
-#####################################
-Pump Setup
-#####################################
+####################################################################
+######################### Pump Setup ###############################
+####################################################################
 """
 pump_magnification = 0.42  
 max_pump_power = 14 # kW
@@ -294,7 +245,7 @@ pump_spot_size = 15 # mm
 final_pump_area = 1e-2*(pump_magnification*pump_spot_size)**2 # cm^2
 
 focal_length1 = 85 # 125
-focal_length2 = 100 if UseTwoTelescopes else 200
+focal_length2 = 100
 lasermedia_dist = 20 # distance from the image planes of the two laser media
 image_plane_to_pump_module_distance = 65 # distance from the pump module to the image plane
 
@@ -310,11 +261,8 @@ print(f"pump intensity = {max_pump_power/final_pump_area:.1f}kW/cm²\n")
 dist_M3_pump_lens = 40
 pump_module_separation = 2*image_distance + 2*dist_M3_pump_lens + lasermedia_dist
 pump_module_lens_dist = object_distance + image_plane_to_pump_module_distance
-pump_module_top_xoffset = pump_module_lens_dist-dist_M3_pump_lens
-pump_module_bot_xoffset = pump_module_top_xoffset
+pump_module_xoffset = pump_module_lens_dist-dist_M3_pump_lens
 
-distance_factor = 6 if UseTwoTelescopes else 2
-distance_lens_to_mirror = (distance_factor*focal_length2-pump_module_separation)/2
 
 beamtop_1 = Ray_Distribution(radius=pump_spot_size/2,angle=1.0*2.08*np.pi/180,wavelength=940E-6, steps=3)
 beamtop_1.draw_dict["color"] = (255/256,255/256,0.0)
@@ -324,12 +272,12 @@ beambot_1.draw_dict["color"] = (255/256,255/256,0.0)
 
 Pump_top = Composition(name="PM19 top")
 Pump_top.set_light_source(beamtop_1)
-Pump_top.pos -= (pump_module_top_xoffset,-pump_module_separation/2,0)
+Pump_top.pos -= (pump_module_xoffset,-pump_module_separation/2,0)
 
 
 Pump_bot = Composition(name="PM19 bot")
 Pump_bot.set_light_source(beambot_1)
-Pump_bot.pos -= (pump_module_bot_xoffset,pump_module_separation/2,0)
+Pump_bot.pos -= (pump_module_xoffset,pump_module_separation/2,0)
 
 Laser_Head_in = Component(name="laser Pump Module PM19 bot")
 stl_file = rf"{thisfolder}\misc_meshes\PM19_2.stl"
@@ -346,10 +294,7 @@ stl_file = rf"{thisfolder}\misc_meshes\Kuehlmount_Alumosilikatglas_d23.stl"
 Kuehlmount_Alumosilikatglas.draw_dict["stl_file"]=stl_file
 Kuehlmount_Alumosilikatglas.freecad_model = load_STL
 
-Kuehlmount2_Alumosilikatglas = Component(name="Kuehlmount Alumosilikatglas d23")
-stl_file = rf"{thisfolder}\misc_meshes\Kuehlmount_Alumosilikatglas_d23.stl"
-Kuehlmount2_Alumosilikatglas.draw_dict["stl_file"]=stl_file
-Kuehlmount2_Alumosilikatglas.freecad_model = load_STL
+Kuehlmount2_Alumosilikatglas = deepcopy(Kuehlmount_Alumosilikatglas)
 
 lens_pump_top = Lens(f=focal_length1, name=f"Pump Lens 1, f={focal_length1}mm")
 lens_pump_top.aperture = 25.4*3
@@ -359,9 +304,12 @@ lens_pump_bot = deepcopy(lens_pump_top)
 lens_pump_bot.name = f"Pump Lens 2, f={focal_length1}mm"
 
 M3_1 = Newport_Mirror(phi=-90, name="3inch mirror", aperture=25.4*3)
+M3_1.Mount.mount_list[0].flip(90)
 M3_2 = Newport_Mirror(phi=90, name="3inch mirror 2", aperture=25.4*3)
+LiMgAS_crystal1 = Cylindric_Crystal(name="LiMgAs", aperture=23, thickness=12)
+LiMgAS_crystal2 = Cylindric_Crystal(name="LiMgAs2", aperture=23, thickness=12)
 
-# Pump_top.rotate((0,0,1), rotate_axis)
+
 Pump_top.pos += offset_axis
 print(f"PM19 top position = ({Pump_top.pos[0]/10:.1f}cm, {Pump_top.pos[1]/10:.1f}cm, {Pump_top.pos[2]/10:.1f}cm)")
 Pump_top.add_on_axis(Laser_Head_in)
@@ -371,6 +319,8 @@ Pump_top.propagate(dist_M3_pump_lens)
 Pump_top.add_on_axis(lens_pump_top)
 Pump_top.propagate(image_distance-6)
 Pump_top.add_on_axis(Kuehlmount_Alumosilikatglas)
+Pump_top.add_on_axis(LiMgAS_crystal1)
+
 
 Pump_bot.pos += offset_axis
 print(f"PM19 bot position = ({Pump_bot.pos[0]/10:.1f}cm, {Pump_bot.pos[1]/10:.1f}cm, {Pump_bot.pos[2]/10:.1f}cm)\n")
@@ -381,6 +331,7 @@ Pump_bot.propagate(dist_M3_pump_lens)
 Pump_bot.add_on_axis(lens_pump_bot)
 Pump_bot.propagate(image_distance-6)
 Pump_bot.add_on_axis(Kuehlmount2_Alumosilikatglas)
+Pump_bot.add_on_axis(LiMgAS_crystal2)
 
 table = Table(name="Right Table", width=900, height=10, thickness=1500)
 table.pos = (0,450,-5)
@@ -398,11 +349,7 @@ Housing2.pos = offset_axis
 Housing2.pos += (3, 0, 80)
 Housing2.draw_dict["color"] = (181/255, 166/255, 66/255)
 
-LiMgAS_crystal1 = Cylindric_Crystal(name="LiMgAs", aperture=23, thickness=12)
-LiMgAS_crystal1.pos += (2,0,0)
 
-LiMgAS_crystal2 = Cylindric_Crystal(name="LiMgAs2", aperture=23, thickness=12)
-LiMgAS_crystal2.pos += (-15, 0, 0)
 
 if __name__ == "__main__":
     if freecad_da:
@@ -412,10 +359,6 @@ if __name__ == "__main__":
         Pump_bot.draw()
         table.draw()
         table2.draw()
-        # Housing.draw()
-        # Housing2.draw()
-        LiMgAS_crystal2.draw()
-        LiMgAS_crystal1.draw()
         # setview()
 
     else:
